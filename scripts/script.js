e0d70aa29ac72bf68f5362d5f17f36d92fb64dbc
Suryakant Chandrakar <785surya@gmail.com>
2022-09-14 01:19:57 +0530

    Co-authored-by: Siddheshwar Chandravanshi <mrsidverse@users.noreply.github.com>

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+
 window.onscroll = function () { myFunction() };
 var hero = document.getElementById("hero");
 var header = document.getElementById("header");
@@ -78,13 +79,3 @@ btn.addEventListener('click', async () => {
         alert('Something went wrong while sharing!')
     }
 });
-
-//Image galleries
-const gallery2 = new Viewer(document.getElementById('images2'));
-const gallery4 = new Viewer(document.getElementById('images4'));
-const gallery5 = new Viewer(document.getElementById('images5'));
-const gallery6 = new Viewer(document.getElementById('images6'));
-const gallery7 = new Viewer(document.getElementById('images7'));
-const gallery8 = new Viewer(document.getElementById('images8'));
-const gallery9 = new Viewer(document.getElementById('images9'));
-const gallery11 = new Viewer(document.getElementById('images11'));
